2e9cabb230ae3774d3acaaf1330fdca2d93e187a
PedroLGon <74836024+PedroL10@users.noreply.github.com>
2022-09-28 10:09:45 -0300

    Ler e salvar arquivos TXT

--- codigo/PROJETO2GRAFOS/ProjetoGrafo/src/Grafos/Main.java
+++ codigo/PROJETO2GRAFOS/ProjetoGrafo/src/Grafos/Main.java
@@ -7,6 +7,47 @@ public class Main {
 	public static void main(String[] args) {
 		
 		ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
+		
+		/*
+		 * Código para ler o arquivo TXT
+		 * 
+		File file = new File("c:\\temp\\FileRead.txt"); 
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch(IOException e) {
+			System.out.println(e.getMessage());
+		} 
+		finally {
+			if(sc != null) {
+				sc.close();
+			}
+		}
+		
+		*/
+		
+		/*
+		 * Codigo para salvar em arquivo TXT
+		 
+		  
+		  String[] lines = new String [] {"A", "B", "C"};
+		  
+		  String path = "c:\\temp\\FileSave.txt";
+		  
+		  try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+			  for (String line : lines) {
+				  bw.write(line);
+				  bw.newLine();
+			  }
+		  } catch (IOException e) {
+			  e.printStackTrace();
+		  }
+		 
+		*/ 		
+		
 		nodeList.add(new GraphNode("A",0));
 		nodeList.add(new GraphNode("B",1));
 		nodeList.add(new GraphNode("C",2));
